7bcc964d137dbee4265a2fe8128a1be7ba6ff9d3
bartek <klys.bartosz@gmail.com>
2016-08-08 19:39:03 +0200

    Problem 2 solved.

---
+++ src/EvenFibonacciNumbers.java
@@ -0,0 +1,20 @@
+/**
+ * Created by bartek on 08.08.16.
+ */
+public class EvenFibonacciNumbers {
+    public void fibonacci(){
+        int limit=4000000;
+        int sum=0;
+        int a=1;
+        int b=1;
+        int c=a+b;
+        while (c<limit) {
+            sum = sum + c;
+
+            a = b + c;
+            b = c + a;
+            c = a + b;
+        }
+        System.out.println(sum);
+    }
+}
--- src/Run.java
+++ src/Run.java
@@ -4,7 +4,7 @@
 public class Run {
 
     public static void main(String[] args) {
-        SmallestMultiple smallestMultiple = new SmallestMultiple();
-        System.out.println(smallestMultiple.calculate());
+        EvenFibonacciNumbers evenFibonacciNumbers = new EvenFibonacciNumbers();
+        evenFibonacciNumbers.fibonacci();
     }
 }
