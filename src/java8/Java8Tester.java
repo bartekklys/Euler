7ed051fb0966062dc6b42d049660146da8d75f7c
Bartosz Klys <Bartosz_Klys@epam,com>
2017-11-03 14:13:20 +0100

    Method Reference Example

---
+++ src/java8/Java8Tester.java
@@ -0,0 +1,20 @@
+package java8;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Java8Tester {
+
+    // Method Reference Example
+
+    public static void main(String args[]) {
+        List<String> names = new ArrayList<>();
+
+        names.add("Mahesh");
+        names.add("Suresh");
+        names.add("Ramesh");
+        names.add("Naresh");
+        names.add("Kalpesh");
+
+        names.forEach(System.out::println);
+    }
+}
